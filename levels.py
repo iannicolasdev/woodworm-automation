bc7715bea1f0e88b150a06c0cb44bcbf767003ad
iannicolasdev <nicolasian002@gmail.com>
2025-05-05 12:07:40 -0300

    modularize: move level data to levels.py

---
+++ levels.py
@@ -0,0 +1,65 @@
+fase1 = [
+    (3, "right"),
+    (1, "up"),
+    (2, "left"),
+    (4, "up"),
+    (1, "right"),
+    (1, "up"),
+    (4, "right"),
+    (1, "down"),
+    (1, "right"),
+    (3, "down"),
+    (2, "left"),
+    (1, "down"),
+    (1, "right")
+]
+
+fase2 = [
+    (1, "right"),
+    (6, "up"),
+    (2, "right"),
+    (2, "down"),
+    (1, "right"),
+    (1, "up"),
+    (1, "right"),
+    (1, "up"),
+    (2, "right"),
+    (1, "down"),
+    (1, "right"),
+    (3, "down"),
+    (2, "left"),
+    (1, "down"),
+    (1, "right"),
+    (1, "down"),
+    (1, "left"),
+    (2, "left"),
+    (1, "up"),
+    (1, "left"),
+    (1, "down"),
+    (1, "left")
+]
+
+fase3 = [
+    (1, "right"),
+    (1, "up"),
+    (4, "right"),
+    (3, "up"),
+    (2, "left"),
+    (1, "down"),
+    (1, "right"),
+    (1, "down"),
+    (1, "left"),
+    (1, "up"),
+    (6, "right")
+]
+
+fase4 = [
+    (1, "right"),
+    (1, "up"),
+    (1, "right"),
+    (3, "up"),
+    (2, "right"),
+    (3, "down"),
+    (2, "right"),
+    (3, "up")
+]
