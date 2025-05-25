0482d888412d46d3e8867642e371770d3a867a3e
iannicolasdev <nicolasian002@gmail.com>
2025-05-25 14:14:29 -0300

    view correction

--- main.py
+++ main.py
@@ -26,53 +26,53 @@ time.sleep(1)
 pydirectinput.press("x")
 time.sleep(8)
 
-# # Primeira fase
-# run_stage(level1)
-# next_stage()
+# Primeira fase
+run_stage(level1)
+next_stage()
 
-# # Segunda fase
-# run_stage(level2)
-# next_stage()
+# Segunda fase
+run_stage(level2)
+next_stage()
 
-# # Terceira fase
-# run_stage(level3)
-# next_stage()
+# Terceira fase
+run_stage(level3)
+next_stage()
 
-# # Quarta fase
-# run_stage(level4)
-# next_stage()
+# Quarta fase
+run_stage(level4)
+next_stage()
 
-# # Quinta fase
-# run_stage(level5)
-# next_stage()
+# Quinta fase
+run_stage(level5)
+next_stage()
 
-# # Sexta fase
-# run_stage(level6)
-# next_stage()
+# Sexta fase
+run_stage(level6)
+next_stage()
 
-# # Setima fase 
-# run_stage(level7)
-# next_stage()
+# Setima fase 
+run_stage(level7)
+next_stage()
 
-# # Oitava fase
-# run_stage(level8)
-# next_stage()
+# Oitava fase
+run_stage(level8)
+next_stage()
 
-# # Nona fase
-# run_stage(level9)
-# next_stage()
+# Nona fase
+run_stage(level9)
+next_stage()
 
-# # Decima fase
-# run_stage(level10)
-# next_stage()
+# Decima fase
+run_stage(level10)
+next_stage()
 
-# # Decima primeira fase
-# run_stage(level11)
-# next_stage()
+# Decima primeira fase
+run_stage(level11)
+next_stage()
 
-# # Decima segunda fase
-# run_stage(level12)
-# next_stage()
+# Decima segunda fase
+run_stage(level12)
+next_stage()
 
 # Decima terceira fase
 run_stage(level13)
